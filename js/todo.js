8ad7d24df904147b517dc12e3d660fe39d72871e
Rowdife <jackpark201313@gmail.com>
2023-01-29 21:41:02 +0900

    last changes(realreal)

--- js/todo.js
+++ js/todo.js
@@ -2,7 +2,6 @@ const letme = new Audio("soundeffect/letmedoitforyoumeme.mp3");
 // soundEffect.volume = 0.5;
 // soundEffect.loop = true;
 // soundEffect.play();
-
 const toDoForm = document.getElementById("todo-form");
 const toDoInput = toDoForm.querySelector("input");
 const toDoList = document.getElementById("todo-list");
@@ -28,27 +27,29 @@ function deleteToDo(event) {
 }
 
 function paintToDo(newTodo) {
-  if (toDos.length < 15) {
-    const li = document.createElement("li");
-    li.id = newTodo.id;
-    const span = document.createElement("span");
-    span.innerText = newTodo.text;
-    const button = document.createElement("button");
-    button.innerHTML = "✅";
-    button.style.background = "transparent";
-    button.style.border = "none";
-    button.addEventListener("click", deleteToDo);
-    li.appendChild(button);
-    li.appendChild(span);
-    toDoList.appendChild(li);
+  if (toDos.length < 10) {
+    setTimeout(function () {
+      const li = document.createElement("li");
+      li.id = newTodo.id;
+      const span = document.createElement("span");
+      span.innerText = newTodo.text;
+      const button = document.createElement("button");
+      button.innerHTML = "✅";
+      button.style.background = "transparent";
+      button.style.border = "none";
+      button.addEventListener("click", deleteToDo);
+      li.appendChild(button);
+      li.appendChild(span);
+      toDoList.appendChild(li);
+    }, 1500);
   } else {
-    alert(`mOrE ThAn 15 tO-dOs?!?!?! let me do it for you....`);
+    alert(`mOrE ThAn 10 tO-dOs?!?!?! let me do it for you....`);
     toDos.pop();
   }
 }
 
 function handleToDoSubmit(event) {
-  if (toDos.length < 14) {
+  if (toDos.length < 9) {
     handleAnimation();
   } else {
     letme.volume = 0.5;
@@ -75,8 +76,22 @@ function handleAnimation() {
   }, 3000);
 }
 
+function handleClear(event) {
+  event.preventDefault();
+  handleAnimation();
+  const li = event.target.previousElementSibling.querySelectorAll("li");
+  setTimeout(function () {
+    for (let i = 0; i < li.length; i++) {
+      li[i].remove();
+    }
+    toDos = [];
+    saveToDos();
+  }, 1500);
+}
+
 // toDoForm.addEventListener("submit", handleAnimation);
 toDoForm.addEventListener("submit", handleToDoSubmit);
+clear.addEventListener("click", handleClear);
 
 const savedToDos = localStorage.getItem(TODOS_KEY);
 
